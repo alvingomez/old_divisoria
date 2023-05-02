83eb387f8bb21eebe860301a3e03ddebe49f970c
Alvin <alvingomez.world@gmail.com>
2023-05-02 22:09:21 +0800

    Bootstrap
    Import Bootstrap to main.jsx  to use Bootstrap package.
    Used the Bootsrap Navbar to create the navbar and align in it on the center by using the Bootstrap container. I've also use the LinkContainer from react router bootstrap fro routing purposes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap'
 // import data from './data';
 
 function App() {   
@@ -9,6 +13,20 @@ function App() {
     <BrowserRouter>
     <div>
       <header>
+        {/* Using React Bootstrap component to create navbar in the header */}
+        <Navbar>
+          {/* Used to center on page or add one within to center contents of 
+              of a fixed or static top navbar  */}
+          <Container>
+              {/* This is a react-router-bootstrap component.
+                  Install separately from react-bootstrap */}
+              <LinkContainer to="/" >
+                <Navbar.Brand>Amazon</Navbar.Brand>
+              </LinkContainer>
+          </Container>
+
+
+        </Navbar>
         <Link to="/" >Divisoria</Link>
       </header>
       <main>
